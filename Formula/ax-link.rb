648c39d7c5d125822937522678dcb0a14d1b7fd5
yshong <yshong@fnfcorp.com>
2026-08-20 14:35:14 +0900

    chore(release): ax-link Formula 1.2.4 갱신

--- Formula/ax-link.rb
+++ Formula/ax-link.rb
@@ -1,25 +1,25 @@
 class AxLink < Formula
   desc "Connect AX Link models to supported AI provider runtimes"
   homepage "https://github.com/F-F-WP/homebrew-public"
-  version "1.2.3"
+  version "1.2.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/F-F-WP/homebrew-public/releases/download/ax-link-v1.2.3/ax-link_1.2.3_darwin_arm64.tar.gz"
-      sha256 "7dd63cc4c89d43fcf03c1c926cdaa9e498a4a3c51c883cfe0df220ce717bb61a"
+      url "https://github.com/F-F-WP/homebrew-public/releases/download/ax-link-v1.2.4/ax-link_1.2.4_darwin_arm64.tar.gz"
+      sha256 "41b29fca54f9bbf1fa87b6144eeda9f3af5af20c75945fe8ae054e05aca64629"
     else
-      url "https://github.com/F-F-WP/homebrew-public/releases/download/ax-link-v1.2.3/ax-link_1.2.3_darwin_amd64.tar.gz"
-      sha256 "e0976376b7fd99f128b3886e468c02d311244ff9d60d80014da437eec6e27f66"
+      url "https://github.com/F-F-WP/homebrew-public/releases/download/ax-link-v1.2.4/ax-link_1.2.4_darwin_amd64.tar.gz"
+      sha256 "7c92b40ab3de4c0046420efda4c3e5ea18cb0a454c7a3c90948206787d316101"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/F-F-WP/homebrew-public/releases/download/ax-link-v1.2.3/ax-link_1.2.3_linux_arm64.tar.gz"
-      sha256 "8aa8953c624e45c0b954c4127311fa60b41d7e75362388d44b8fde2375eb5dfb"
+      url "https://github.com/F-F-WP/homebrew-public/releases/download/ax-link-v1.2.4/ax-link_1.2.4_linux_arm64.tar.gz"
+      sha256 "c4cd562f797bfc5b1b36c9badad384fb5c8248ec82d1ea536deba057a0ef1925"
     else
-      url "https://github.com/F-F-WP/homebrew-public/releases/download/ax-link-v1.2.3/ax-link_1.2.3_linux_amd64.tar.gz"
-      sha256 "f40bc2593adc811614baa3319aaca7586465189bbf12b04576477f16e7315f49"
+      url "https://github.com/F-F-WP/homebrew-public/releases/download/ax-link-v1.2.4/ax-link_1.2.4_linux_amd64.tar.gz"
+      sha256 "9084c71bdc811b704b1c22ff1e570c8173ef5e5e5e1090ee69e4cb405d9ab5e5"
     end
   end
 
